50b6b25b339dc795877d34c412dbd1ff78cf1118
qhy040404 <45379733+qhy040404@users.noreply.github.com>
2022-07-24 15:58:47 +0800

    Create utils.js

---
+++ result-dialog/js/utils.js
@@ -0,0 +1,9 @@
+const button=document.getElementById("insertv"); 
+       button.onclick = function () { 
+        let a=document.getElementById("innerv").value;
+        output(a); 
+    }
+    function output(str) { 
+        const b=document.getElementById("zy"); 
+        b.value=str; 
+        }
