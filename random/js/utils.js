21a7a7d301c13a23489c915b98f31f9a9eceabec
qhy040404 <qhy040404@163.com>
2022-12-22 13:38:14 +0800

    Finish random

--- random/js/utils.js
+++ random/js/utils.js
@@ -1,7 +1,43 @@
 /**
  * @param max Max number
+ * @param fix Fixed point
  * @return random data
  */
-function generateRandomData(max) {
-    return Math.random() * parseFloat(max)
+function generateRandomData(max, fix) {
+    let rand = mRandom() * parseFloat(max)
+    if (fix == "") {
+        return rand
+    } else {
+        return rand.toFixed(parseInt(fix))
+    }
+}
+
+function mRandom() {
+    let origRandom = Math.random()
+    let now = Date.now()
+    // noinspection JSCheckFunctionSignatures
+    let pNow = Date.parse(new Date())
+    let m = 1.0;
+
+    let cryptoRan = crypto.getRandomValues(new Uint32Array(1))[0] / 10000000000
+
+    for (let i = 0; i < 5; i++) {
+        m *= (now - i - pNow - i)
+        if (m < 0) m = 0 - m
+        while (m > 1) {
+            m /= 10
+        }
+        if (i % 2 == 0) {
+            m += cryptoRan
+        } else {
+            m += cryptoRan / 2
+        }
+    }
+    if (m < 0.5) {
+        m += Math.random() * 0.5
+    }
+    while (m > 1) {
+        m -= cryptoRan / 3
+    }
+    return origRandom * m
 }
